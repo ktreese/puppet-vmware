5e4b448f34d3ab859288b68788232f1ebce14a36
ktreese <ktreese@gmail.com>
2015-11-21 00:24:41 -0600

    Update vm.pp

--- vm.pp
+++ vm.pp
@@ -1,10 +1,12 @@
-vsphere_vm { '/<datacenter>/vm/<virtual_server_name>':
-  ensure        => present,
-  source        => '/<datacenter>/vm/<template_name>',
-  source_type   => 'template',
-  resource_pool => '192.168.1.2',
+vsphere_vm { '/REESE/vm/kris':
+  ensure             => 'running',
+  source             => '/REESE/vm/redhat6.5',
+  source_type        => 'vm',
+  resource_pool      => '192.168.1.2',
+  customization_spec => 'puppet_vsphere_vm',
   create_command => {
-    command => 'touch /tmp/test',
+    command => '/bin/touch',
+    arguments => '/tmp/test',
     working_directory => '/',
     user => 'root',
     password => 'password',
