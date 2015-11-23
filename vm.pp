48420fc311d2e5e07f93516a86eb967376fbd742
ktreese <ktreese@gmail.com>
2015-11-23 12:13:20 -0600

    Install puppet agent
    During some tests, curl timeouts occurred.  I speculate that the customization_spec wasn't fully applied yet, therefore the IP had not yet been assigned before vsphere_vm executed the command.  This boils down to a timing issue between availability of an active network connection and the timing of the retry attempts of the vsphere_vm guest operations agent.

--- vm.pp
+++ vm.pp
@@ -1,12 +1,12 @@
 vsphere_vm { '/REESE/vm/kris':
   ensure             => 'running',
   source             => '/REESE/vm/redhat6.5',
-  source_type        => 'vm',
+  source_type        => 'template',
   resource_pool      => '192.168.1.2',
   customization_spec => 'puppet_vsphere_vm',
   create_command => {
-    command => '/bin/touch',
-    arguments => '/tmp/test',
+    command => '/usr/bin/curl',
+    arguments => '-k https://puppetmaster:8140/packages/current/install.bash | /bin/bash >/tmp/agent.install.log 2>&1',
     working_directory => '/',
     user => 'root',
     password => 'password',
